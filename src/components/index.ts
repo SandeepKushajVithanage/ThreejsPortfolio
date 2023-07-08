fe9372a19fc247d20c6af28c777259b186f3b881
SandeepKushajVithanage <sandeepkushaj3@gmail.com>
2023-07-08 16:43:56 +0530

    first commit

---
+++ src/components/index.ts
@@ -0,0 +1,29 @@
+import About from "./About";
+import Contact from "./Contact";
+import Experience from "./Experience";
+import Feedbacks from "./Feedbacks";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Tech from "./Tech";
+import Works from "./Works";
+import {
+  EarthCanvas,
+  BallCanvas,
+  ComputersCanvas,
+  StarsCanvas,
+} from "./canvas";
+
+export {
+  Hero,
+  Navbar,
+  About,
+  Tech,
+  Experience,
+  Works,
+  Feedbacks,
+  Contact,
+  EarthCanvas,
+  BallCanvas,
+  ComputersCanvas,
+  StarsCanvas,
+};
